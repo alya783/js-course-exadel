84d2a1d604d2e9a4875eeac87b2e4023fc676b36
alya783 <76521831+alya783@users.noreply.github.com>
2022-02-17 15:20:10 +0100

    Update task3.js

--- HW#1/task3.js
+++ HW#1/task3.js
@@ -2,12 +2,11 @@ const prices = [64, 7556, 345, 7556, 345, 7556, 345, 7556, 433, 345, 756, 123, 9
 
 function getPrice(arr) {
     let total = 0;
-
     for (item of arr) {
         total += item;
     }
-
     let avg = Math.floor(total / arr.length);
+    
     console.log(`Total: ${total} hryvnias, average price of goods ${avg} hryvnias `);
 }
 
