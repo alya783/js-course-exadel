aaf072545e925dd8a623bbce966e6842ed47f15f
Alina Strashevichutse <astrashevichute@gmail.com>
2022-02-22 15:44:02 +0100

    fix solution

--- HW#2/task3.js
+++ HW#2/task3.js
@@ -1,7 +1,7 @@
 const arr =  ["НПП Прізвище Кіл-ть Приблизно","______________________в_ базіпо_Україні", "0-rt-rgfr", "8484fjdfkf", "МЕЛЬНИК","ШЕВЧЕНКО","БОЙКО","КОВАЛЕНКО","БОНДАРЕНКО","ТКАЧЕНКО","КОВАЛЬЧУК","КРАВЧЕНКО","ОЛІЙНИК"]
 
 function getValidLastNames(array){
-	return array.filter(item => item === item.toUpperCase());
+	return array.filter(item => item === item.toUpperCase() && item.match(/^[А-Я]+$/));
 }
 
 console.log(getValidLastNames(arr));
